90a1ca8dbea9a5381d278cfbc9cd93ca8ef65e3e
imsidkg <siddharth8271skg2@gmail.com>
2024-08-20 18:32:40 +0530

    test

--- app/main.ts
+++ app/main.ts
@@ -4,31 +4,54 @@ const pattern = args[3];
  export const inputLine: string = await Bun.stdin.text();
 
 function matchPattern(inputLine: string, pattern: string): boolean {
-  if (pattern.length === 1) {
-    return inputLine.includes(pattern);
-  }
-  else if(pattern === "\\d"){
-    console.log('reched 1')
-    return /\d/g.test(inputLine)
-   }
-   else if (pattern === '\\w') {
-    console.log('reched 2')
-    return /\w/g.test(inputLine)
-   }
-   else if (pattern[0] === "[" && pattern[pattern.length - 1] === "]" && pattern[1] === "^") {
-     console.log('reched 4')
-     const negGroupChars = pattern.slice(2, pattern.length - 1); // Extract characters after ^ and before ]
-     return !Array.from(negGroupChars).some((negGroupChar) => inputLine.includes(negGroupChar))
-    }
-    else if (pattern.startsWith('[') && pattern.endsWith(']')){
-     const chars = pattern.slice(0,pattern.length-1);
+  let i=0 ;
+  let j=0;
+  // if (pattern.length === 1) {
+  //   return inputLine.includes(pattern);
+  // }
+  // else if(pattern === "\\d"){
+  //   console.log('reched 1')
+  //   return /\d/g.test(inputLine)
+  //  }
+  //  else if (pattern === '\\w') {
+  //   console.log('reched 2')
+  //   return /\w/g.test(inputLine)
+  //  }
+  //  else if (pattern[0] === "[" && pattern[pattern.length - 1] === "]" && pattern[1] === "^") {
+  //    console.log('reched 4')
+  //    const negGroupChars = pattern.slice(2, pattern.length - 1); 
+  //    return !Array.from(negGroupChars).some((negGroupChar) => inputLine.includes(negGroupChar))
+  //   }
+  //   else if (pattern.startsWith('[') && pattern.endsWith(']')){
+  //    const chars = pattern.slice(0,pattern.length-1);
      
-     return Array.from(chars).some((char) => inputLine.includes(char))
+  //    return Array.from(chars).some((char) => inputLine.includes(char))
+  //   }
+  while (i<inputLine.length && j<pattern.length-1) {
+    const currentPatternCharacter = pattern[j] ;
+     
+    if(currentPatternCharacter === '//') {
+      const nextPatternCharacter = pattern[j+1];
+      if(nextPatternCharacter === 'd') {
+        if(!/\d/.test(inputLine[i])) return false
+        j+=2 ;
+    }
+    else if(nextPatternCharacter === 'w') {
+      if(!/\w/.test(inputLine[i])) return false
+      j+=2;
     }
 
-  else {
-    throw new Error(`Unhandled pattern: ${pattern}`);
+    }
+    else {
+      if(pattern[j] !== inputLine[i]) return false
+      j +=1;
+    }
+     i+=1;
+
+
   }
+  return i === inputLine.length && j == pattern.length;
+ 
 }
 
 if (args[2] !== "-E") {
@@ -36,7 +59,6 @@ if (args[2] !== "-E") {
   process.exit(1);
 }
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
@@ -45,3 +67,4 @@ if (matchPattern(inputLine, pattern)) {
 } else {
   process.exit(1);
 }
+  
